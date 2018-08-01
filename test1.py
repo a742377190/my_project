0e240d1f8e0cb719109cf8348afb19574743e938
zhangfan <zhangfan12388@163.com>
2018-08-01 21:19:59 +0800

    ---submit code

---
+++ test1.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+
+
+fig = plt.figure()
+ax = Axes3D(fig)
+
+x = np.arange(-4,4,0.25)
+y = np.arange(-4,4,0.25)
+
+X,Y = np.meshgrid(x,y)
+
+Z = np.sin(np.sqrt(X**2+Y**2))
+ax.plot_surface(X,Y,Z,rstride = 1,cstride = 1,cmap='rainbow')
+ax.contour(X,Y,Z,zdim='z',offset = -2 ,cmap='rainbow')
+
+ax.set_zlim(-2,2)
+plt.show()
